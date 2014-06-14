60c5bdb78b7909446f91c6963414c889bda314d5
Alfonso Jiménez <yo@alfonsojimenez.com>
2014-06-14 19:44:45 +0200

    Moving files

--- install.sh
+++ install.sh
@@ -10,14 +10,14 @@ unlink ~/.bin
 unlink ~/.zshrc
 unlink ~/.kde/share/apps/konsole/Alfie.colorscheme
 
-ln -s "$DIR/.tmux.conf" ~/.tmux.conf
-ln -s "$DIR/.vimrc" ~/.vimrc
-ln -s "$DIR/.vim" ~/.vim
-ln -s "$DIR/.gemrc" ~/.gemrc
-ln -s "$DIR/.gitconfig" ~/.gitconfig
-ln -s "$DIR/.bundle" ~/.bundle
-ln -s "$DIR/.bin" ~/.bin
-ln -s "$DIR/.zshrc" ~/.zshrc
-ln -s "$DIR/.konsole/Alfie.colorscheme" ~/.kde/share/apps/konsole/
+ln -s "$DIR/tmux.conf" ~/.tmux.conf
+ln -s "$DIR/vimrc" ~/.vimrc
+ln -s "$DIR/vim" ~/.vim
+ln -s "$DIR/gemrc" ~/.gemrc
+ln -s "$DIR/gitconfig" ~/.gitconfig
+ln -s "$DIR/bundle" ~/.bundle
+ln -s "$DIR/bin" ~/.bin
+ln -s "$DIR/zshrc" ~/.zshrc
+ln -s "$DIR/konsole/Alfie.colorscheme" ~/.kde/share/apps/konsole/
 
 echo "All dotfiles have been installed :)"
